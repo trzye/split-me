1763cdc5ead2c58684875390f7ff5eb3f0ab7652
Michał Jereczek <michaljereczek@gmail.com>
2018-12-21 21:22:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,19 +2,19 @@ self.addEventListener('install', function(e) {
     e.waitUntil(
         caches.open('split-me').then(function(cache) {
             return cache.addAll([
-                "/",
-                "/assets/i18n/en.json",
-                "/3rdpartylicenses.txt",
-                "/favicon.ico",
-                "/icon.png",
-                "/index.html",
-                "/index.js",
-                "/main.js",
-                "/manifest.json",
-                "/polyfills.js",
-                "/runtime.js",
-                "/styles.css",
-                "/sw.js"
+                "",
+                "assets/i18n/en.json",
+                "3rdpartylicenses.txt",
+                "favicon.ico",
+                "icon.png",
+                "index.html",
+                "index.js",
+                "main.js",
+                "manifest.json",
+                "polyfills.js",
+                "runtime.js",
+                "styles.css",
+                "sw.js"
             ]);
         })
     );
