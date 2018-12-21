0475d6a4c4e414033c4094df390732b8bcb1c703
m.jereczek <m.jereczek@uw.edu.pl>
2018-12-21 21:11:08 +0100

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const installButton = document.getElementById('pwa-install');
+
+let installPrompt = null;
+installButton.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (prompt) => {
+    installPrompt = prompt;
+    installButton.style.display = 'block';
+    installButton.addEventListener('click', (e) => {
+        installButton.style.display = 'none';
+        installPrompt.prompt();
+        installPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log(' User accepted installation.');
+            } else {
+                console.log(' User refused installation.');
+            }
+            deferredPrompt = null;
+        });
+    });
+});
+
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register("sw.js").then(function() {
+        console.log(" Service worker is enabled.")
+    });
+}
---
+++ sw.js
@@ -0,0 +1,30 @@
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('split-me').then(function(cache) {
+            return cache.addAll([
+                "/",
+                "/assets/i18n/en.json",
+                "/3rdpartylicenses.txt",
+                "/favicon.ico",
+                "/icon.png",
+                "/index.html",
+                "/index.js",
+                "/main.js",
+                "/manifest.json",
+                "/polyfills.js",
+                "/runtime.js",
+                "/styles.css",
+                "/sw.js"
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log(e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
